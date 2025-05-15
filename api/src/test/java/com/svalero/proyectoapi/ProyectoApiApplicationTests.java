d4fe862f9657c859737a955b70c648c3fb0daec1
jbermejo14 <jpbermejobramley@gmail.com>
2025-05-15 15:12:27 +0200

    Creado esqueleto del proyecto

---
+++ api/src/test/java/com/svalero/proyectoapi/ProyectoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svalero.proyectoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
